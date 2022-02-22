f880ff1aea2d7b372c9f28d0935f774372b95c51
G-Taylor <graeme.taylor@hotmail.com>
2022-02-22 21:50:09 +0000

    added loop to display results

--- app/app.py
+++ app/app.py
@@ -1,12 +1,12 @@
 from flask import Flask, render_template
-
+from data_retrieval.Main import get_fixture_over_2
 app = Flask(__name__)
 
 
 @app.route('/')
 def hello_world():
-    # results = get_fixture_over_2()
-    return render_template('index.html')
+    results = get_fixture_over_2()
+    return render_template('index.html', results=results)
 
 
 if __name__ == '__main__':
