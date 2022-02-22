f3f6934ccb23db7a22fd2fe51fa1b54354deed8b
G-Taylor <graeme.taylor@hotmail.com>
2022-02-22 21:45:26 +0000

    added basic css and html to frontend

--- app/app.py
+++ app/app.py
@@ -1,16 +1,13 @@
 from flask import Flask, render_template
-from data_retrieval.Main import get_fixture_over_2
-
 
 app = Flask(__name__)
 
 
 @app.route('/')
 def hello_world():
-
     # results = get_fixture_over_2()
-    return get_fixture_over_2()
+    return render_template('index.html')
 
 
 if __name__ == '__main__':
-    app.run()
+    app.run(debug=True)
