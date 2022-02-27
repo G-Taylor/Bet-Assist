befe13b63feaa6467a07cec54f5a1cc61a0dfa52
G-Taylor <graeme.taylor@hotmail.com>
2022-02-27 20:41:21 +0000

    working on abstraction

--- app/app.py
+++ app/app.py
@@ -1,5 +1,6 @@
 from flask import Flask, render_template
 from data_retrieval.Main import get_fixture_over_2
+from data_retrieval.test import convert_stats
 
 app = Flask(__name__)
 
@@ -7,6 +8,7 @@ app = Flask(__name__)
 @app.route('/')
 def hello_world():
 
+    convert_stats()
     results = get_fixture_over_2()
 
     return render_template('index.html', results=results)
