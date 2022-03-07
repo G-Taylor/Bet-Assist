bcde66913b37b052bd6e42498359c389c284c11a
G-Taylor <graeme.taylor@hotmail.com>
2022-03-07 22:57:54 +0000

    further removal of old unused code

--- data_retrieval/game_stats_retrieval.py
+++ data_retrieval/game_stats_retrieval.py
@@ -1,25 +1,3 @@
-# from data_retrieval.test import all_results_dict
-
-
-# def get_last_results(result):
-#     team_list = []
-#
-#     for team in all_results_dict:
-#         result_list = []
-#         result_list += all_results_dict[team][0]
-#         result_list += all_results_dict[team][1]
-#         # total_results = result_list + result_list2
-#
-#         if result == 'won' and result_list.count('W') >= 4:
-#             team_list.append(team)
-#         elif result == 'lost' and result_list.count('L') >= 3:
-#             team_list.append(team)
-#         elif result == 'draw' and result_list.count('D') >= 3:
-#             team_list.append(team)
-#
-#     return team_list
-
-
 def get_total_goals(team_name, goal_dict):
     try:
         goals = sum(goal_dict[team_name][1])
