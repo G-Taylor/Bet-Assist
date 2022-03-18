88c0089a84dd6f0b832cbe4e5d590ca2a03cadce
G-Taylor <graeme.taylor@hotmail.com>
2022-03-18 23:13:55 +0000

    getting ready for new pages to be added

--- app/app.py
+++ app/app.py
@@ -35,5 +35,34 @@ def index():
                            )
 
 
+# App Route for the index page of the application
+# @app.route('/allteams', methods=['GET', 'POST'])
+# def index():
+#     current_league = "Premiership"
+#     league = f"{WEBSITE_URL}{leagues[current_league]}"
+#
+#     if request.method == "POST":
+#         current_league = request.form.get('league')
+#         league = f"{WEBSITE_URL}{leagues[current_league]}"
+#
+#     reset_all_value_stores()
+#     get_games_played(f"{league}results/")
+#     get_fixtures(f"{league}fixtures/")
+#     get_current_standings(f"{league}standings/")
+#     convert_data()
+#
+#     total_goals_scored_dict = merge_dict(home_goals_scored_dict, away_goals_scored_dict)
+#     total_goals_conceded_dict = merge_dict(home_goals_conceded_dict, away_goals_conceded_dict)
+#     all_results_dict = merge_dict(away_team_results_dict, home_team_results_dict)
+#
+#     results = get_suggested_matches(total_goals_scored_dict, total_goals_conceded_dict, all_results_dict)
+#
+#     return render_template('index.html',
+#                            results=results,
+#                            current_league=current_league,
+#                            current_standings=current_standings
+#                            )
+
+
 if __name__ == '__main__':
     app.run(debug=True)
