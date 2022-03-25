90fd992cf36d970f640fe5e143e62b8c8e24ab6c
G-Taylor <graeme.taylor@hotmail.com>
2022-03-25 16:51:49 +0000

    Big Update: - Changed algorithm to look for games with high total average goals, based on stats recorded for the month. - Fixed a bug in the code where no results were being returned for a league when they didnt have any upcoming away fixtures. - Updated front end to show total average goals

--- data_retrieval/Main.py
+++ data_retrieval/Main.py
@@ -49,20 +49,22 @@ def get_suggested_matches(total_goals_scored_dict, total_goals_conceded_dict, al
             home_index = home_fixture_list.index(team)
             away_index = 0
             for team2 in teams_over_2_goals:
-                away_index = away_fixture_list.index(team2)
-                if home_index == away_index:
-                    suggested_matches[home_index] = {}
-                    suggested_matches[home_index]['home_team'] = team
-                    suggested_matches[home_index]['home_results'] = all_results_dict[team]
-                    suggested_matches[home_index]['home_average_goals'] = f'{teams_over_2_goals[team]:.2f}'
-                    suggested_matches[home_index]['home_goals_scored'] = f'{teams_over_2_goals_scored[team]:.2f}'
-                    suggested_matches[home_index]['home_goals_conceded'] = f'{teams_over_2_goals_conceded[team]:.2f}'
+                if team2 in away_fixture_list:
+                    away_index = away_fixture_list.index(team2)
+                    if home_index == away_index:
+                        suggested_matches[home_index] = {}
+                        suggested_matches[home_index]['home_team'] = team
+                        suggested_matches[home_index]['home_results'] = all_results_dict[team]
+                        suggested_matches[home_index]['home_average_goals'] = f'{teams_over_2_goals[team]:.2f}'
+                        suggested_matches[home_index]['home_goals_scored'] = f'{teams_over_2_goals_scored[team]:.2f}'
+                        suggested_matches[home_index]['home_goals_conceded'] = f'{teams_over_2_goals_conceded[team]:.2f}'
 
-                    suggested_matches[home_index]['away_team'] = team2
-                    suggested_matches[home_index]['away_results'] = all_results_dict[team2]
-                    suggested_matches[home_index]['away_average_goals'] = f'{teams_over_2_goals[team2]:.2f}'
-                    suggested_matches[home_index]['away_goals_scored'] = f'{teams_over_2_goals_scored[team2]:.2f}'
-                    suggested_matches[home_index]['away_goals_conceded'] = f'{teams_over_2_goals_conceded[team2]:.2f}'
+                        suggested_matches[home_index]['away_team'] = team2
+                        suggested_matches[home_index]['away_results'] = all_results_dict[team2]
+                        suggested_matches[home_index]['away_average_goals'] = f'{teams_over_2_goals[team2]:.2f}'
+                        suggested_matches[home_index]['away_goals_scored'] = f'{teams_over_2_goals_scored[team2]:.2f}'
+                        suggested_matches[home_index]['away_goals_conceded'] = f'{teams_over_2_goals_conceded[team2]:.2f}'
+                        suggested_matches[home_index]['total_average_goals'] = f'{teams_over_2_goals[team] + teams_over_2_goals[team2]:.2f}'
         except ValueError:
             pass
 
