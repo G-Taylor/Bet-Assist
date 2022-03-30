bb3041d4e8e3ad4dd006ac3fef711fa6d91f1bf7
G-Taylor <graeme.taylor@hotmail.com>
2022-03-30 23:18:56 +0100

    added logic to frontend to not display league selection column when on the home page

--- app/app.py
+++ app/app.py
@@ -9,30 +9,7 @@ app = Flask(__name__)
 # App Route for the index page of the application
 @app.route('/', methods=['GET', 'POST'])
 def index():
-    current_league = "Premiership"
-    league = f"{WEBSITE_URL}{leagues[current_league]}"
-
-    if request.method == "POST":
-        current_league = request.form.get('league')
-        league = f"{WEBSITE_URL}{leagues[current_league]}"
-
-    reset_all_value_stores()
-    get_games_played(f"{league}results/")
-    get_fixtures(f"{league}fixtures/")
-    get_current_standings(f"{league}standings/")
-    convert_data()
-
-    total_goals_scored_dict = merge_dict(home_goals_scored_dict, away_goals_scored_dict)
-    total_goals_conceded_dict = merge_dict(home_goals_conceded_dict, away_goals_conceded_dict)
-    all_results_dict = merge_dict(away_team_results_dict, home_team_results_dict)
-
-    results = get_suggested_matches(total_goals_scored_dict, total_goals_conceded_dict, all_results_dict)
-
-    return render_template('all_games.html',
-                           results=results,
-                           current_league=current_league,
-                           current_standings=current_standings
-                           )
+    return render_template('index.html')
 
 
 # App Route for the index page of the application
