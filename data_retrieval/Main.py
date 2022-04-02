0d8dba94c11b4e78067af712da438ce65fbaa0a5
G-Taylor <graeme.taylor@hotmail.com>
2022-04-02 22:06:14 +0100

    updated algorithm

--- data_retrieval/Main.py
+++ data_retrieval/Main.py
@@ -64,7 +64,7 @@ def get_suggested_matches(total_goals_scored_dict, total_goals_conceded_dict, al
                         suggested_matches[home_index]['away_average_goals'] = f'{teams_over_2_goals[team2]:.2f}'
                         suggested_matches[home_index]['away_goals_scored'] = f'{teams_over_2_goals_scored[team2]:.2f}'
                         suggested_matches[home_index]['away_goals_conceded'] = f'{teams_over_2_goals_conceded[team2]:.2f}'
-                        suggested_matches[home_index]['total_average_goals'] = f'{teams_over_2_goals[team] + teams_over_2_goals[team2]:.2f}'
+                        suggested_matches[home_index]['total_average_goals'] = f'{(teams_over_2_goals[team] + teams_over_2_goals[team2]) /2:.2f}'
         except ValueError:
             pass
 
