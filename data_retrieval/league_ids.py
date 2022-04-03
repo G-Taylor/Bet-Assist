91eb67d8d1aa29f34f1e67259f5a7b4f19390e93
G-Taylor <graeme.taylor@hotmail.com>
2022-04-03 23:49:20 +0100

    working on adding league table for all leagues

---
+++ data_retrieval/league_ids.py
@@ -0,0 +1,15 @@
+league_ids = {
+    'Premiership': 6135,
+    'Championship': 'england/championship/',
+    'League 1': 'england/league-one/',
+    'League 2': 'england/league-two/',
+    'National League': 'england/national-league/',
+    'SCOT': 'scotland/premiership/',
+    'La Liga': 'spain/laliga/',
+    'Bundesliga': 'germany/bundesliga/',
+    'Serie A': 'italy/serie-a/',
+    'Ligue 1': 'france/ligue-1/',
+    'Eredivisie': 'netherlands/eredivisie/',
+    'Champions League': 'uefa/champions-league/',
+    'Europa League': 'uefa/europa-league/',
+}
