36beb02078710e0dcc99bc5a4cbc61aabd290d8d
G-Taylor <graeme.taylor@hotmail.com>
2022-07-25 16:50:12 +0100

    added tests for urls

--- app/tests/test_url.py
+++ app/tests/test_url.py
@@ -1,14 +1,29 @@
 import unittest
 import requests
+from app.data_retrieval.league_urls import leagues
 
 
-class UrlTestCase(unittest.TestCase):
+class UrlTests(unittest.TestCase):
 
     # Test base url for scraping data returns a 200 response
     def test_base_web_url(self):
         url = 'https://www.skysports.com/'
         page = requests.get(url)
         self.assertEqual(page.status_code, 200)
+
+    def test_league_result_urls(self):
+        for league in leagues:
+            print(f'Testing league results url for {league}')
+            url = f'https://www.skysports.com/{leagues[league]}-results'
+            page = requests.get(url)
+            self.assertEqual(page.status_code, 200)
+
+    def test_league_fixture_urls(self):
+        for league in leagues:
+            print(f'Testing league fixtures url for {league}')
+            url = f'https://www.skysports.com/{leagues[league]}-fixtures'
+            page = requests.get(url)
+            self.assertEqual(page.status_code, 200)
 
 
 if __name__ == '__main__':
