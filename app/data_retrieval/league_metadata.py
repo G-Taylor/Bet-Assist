09de386757d8ed1b225108eb90b2d6ff1281888d
G-Taylor <graeme.taylor@hotmail.com>
2022-08-16 23:02:09 +0100

    working on consolidating all league metadata into one file, to reduce the number of imports

---
+++ app/data_retrieval/league_metadata.py
@@ -0,0 +1,103 @@
+WEBSITE_URL = 'https://www.skysports.com/'
+leagues = {
+    'premiership': {
+        'id': 7704,
+        'logo': 'premier-league.png',
+        'url': 'premier-league',
+    },
+    'championship': {
+        'id': 7593,
+        'logo': 'championship.png',
+        'url': 'championship',
+    },
+    'league 1': {
+        'id': 7570,
+        'logo': 'league1.png',
+        'url': 'league-1',
+    },
+    'league 2': {
+        'id': 7574,
+        'logo': 'league2.png',
+        'url': 'league-2',
+    },
+    'national league': {
+        'id': 7729,
+        'logo': 'national.png',
+        'url': '',
+    },
+    'scottish premiership': {
+        'id': 7494,
+        'logo': 'scotprem.png',
+        'url': 'scottish-premier',
+    },
+    'scottish championship': {
+        'id': 7498,
+        'logo': 'scotchamp.png',
+        'url': 'scottish-championship',
+    },
+    'la liga': {
+        'id': 7665,
+        'logo': 'laliga.png',
+        'url': 'la-liga',
+    },
+    'bundesliga': {
+        'id': 7664,
+        'logo': 'bundesliga.png',
+        'url': 'bundesliga',
+    },
+    'serie a': {
+        'id': 7608,
+        'logo': 'seriea.png',
+        'url': 'serie-a',
+    },
+    'serie b': {
+        'id': 7864,
+        'logo': 'serieb.png',
+        'url': '',
+    },
+    'ligue 1': {
+        'id': 7500,
+        'logo': 'ligue1.png',
+        'url': 'ligue-1',
+    },
+    'eredivisie': {
+        'id': 7482,
+        'logo': 'eredivisie.png',
+        'url': 'eredivisie',
+    },
+    'jupiler pro league': {
+        'id': 7544,
+        'logo': 'jupiler.png',
+        'url': '',
+    },
+    'mls': {
+        'id': 6969,
+        'logo': 'mls.png',
+        'url': 'mls',
+    },
+    'hungarian nb1': {
+        'id': 7967,
+        'logo': 'nb1.png',
+        'url': '',
+    },
+    'argentina superliga': {
+        'id': 7172,
+        'logo': 'superliga.png',
+        'url': '',
+    },
+    'champions league': {
+        'id': 7455,
+        'logo': 'championsleague.png',
+        'url': 'champions-league',
+    },
+    'europa league': {
+        'id': 7985,
+        'logo': 'europaleague.png',
+        'url': 'europa-league',
+    },
+    'europa conference league': {
+        'id': 7480,
+        'logo': 'europaconferenceleague.png',
+        'url': 'europa-conference-league',
+    },
+}
