16633090523f73cfc02fc5671a9faefb46382fc4
G-Taylor <graeme.taylor@hotmail.com>
2022-09-05 23:39:04 +0100

    updating docstrings

--- app/data_retrieval/check_both_teams_to_score.py
+++ app/data_retrieval/check_both_teams_to_score.py
@@ -5,10 +5,10 @@ class CheckBothTeamsToScore:
     @staticmethod
     def check_btts(matches):
         """
-        Function to set the BTTS boolean value for each match, based on predefined rules
+        Function to set the BTTS boolean value for each match, based on the predefined rules
 
         :param matches:
-        :return:
+        :return: updated matches dictionary, with boolean value for btts
         """
         for match in matches:
             if matches[match]['home_goals_scored'] >= 1.5 and matches[match]['home_goals_conceded'] > 0.8:
@@ -19,6 +19,16 @@ class CheckBothTeamsToScore:
 
     @staticmethod
     def get_btts_rating(home_goals_scored, home_goals_conceded, away_goals_scored, away_goals_conceded):
+        """
+        Function that checks all of a games previous matches to see if both teams scored in each match, and returns the
+        total percentage of those matches that were btts.
+
+        :param home_goals_scored:
+        :param home_goals_conceded:
+        :param away_goals_scored:
+        :param away_goals_conceded:
+        :return: float btts rating for each team in the specified league
+        """
         btts_rating = {}
         league_team_keys = []
         [league_team_keys.append(team) for team in home_goals_scored.keys()]
@@ -50,6 +60,13 @@ class CheckBothTeamsToScore:
 
     @staticmethod
     def assign_btts_ratings(matches, btts_rating):
+        """
+        Function that assigns the btts rating for each team into the matches dictionary
+
+        :param matches: dictionary containing all match details
+        :param btts_rating: float value showing the percentage of games that were btts for a team
+        :return: updated matches dictionary containing btts_rating for home team, away team and match average
+        """
         for match in matches:
             home_team_rating = btts_rating.get(matches[match]['home_team'])
             away_team_rating = btts_rating.get(matches[match]['away_team'])
